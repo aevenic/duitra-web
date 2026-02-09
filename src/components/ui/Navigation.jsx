bfabfdfa48ddd0d485b487dbe318f51432313f3e
Salman Setiawan <salmansetiawan88@gmail.com>
2026-02-09 11:15:40 +0800

    update navigation

--- src/components/ui/Navigation.jsx
+++ src/components/ui/Navigation.jsx
@@ -1,8 +1,11 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import Button from "./Button"
 import LanguageDropdown from "./LanguageDropdown"
 
 const Navigation = () => {
+	const location = useLocation();
+	const hideDownload = ["/privacy-policy", "/terms-of-service"].includes(location.pathname);
+
 	return (
 		<div className="fixed top-0 w-full z-50 bg-[#0c0c0c]/70 backdrop-blur-md border-b border-white/3">
 			<nav className='flex justify-between items-center w-full px-4 py-2.5'>
@@ -11,11 +14,13 @@ const Navigation = () => {
 				</Link>
 
 				<div className="flex items-center gap-x-2">
-					<Button
-						type='nav'
-						url='/download'
-						label="Download App"
-					/>
+					{!hideDownload && (
+						<Button
+							type='nav'
+							url='/download'
+							label="Download App"
+						/>
+					)}
 					<LanguageDropdown />
 				</div>
 			</nav>
